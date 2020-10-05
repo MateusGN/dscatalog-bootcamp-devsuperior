bb838ab30a062220ac43d0e6e9c86b9886db7e82
MateusKha <mateusgnascimento@outlook.com>
2020-10-04 21:46:01 -0300

    Product entity, SQL script

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,6 +1,5 @@
 package com.devsuperior.dscatalog.services;
 
-import java.util.List;
 import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
--- backend/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO tb_category (name, created_At) VALUES ('Books', NOW());
-INSERT INTO tb_category (name, created_At) VALUES ('Eletronics', NOW());
-INSERT INTO tb_category (name, created_At) VALUES ('Computers', NOW());
